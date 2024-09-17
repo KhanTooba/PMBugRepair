b86ae0ea29727c9ef2ae3eacbf859d214eeb761a
KhanTooba <toobakha@usc.edu>
2024-09-16 17:13:46 -0700

    added support for PMDK

--- Transform/PM/pm.cpp
+++ Transform/PM/pm.cpp
@@ -72,14 +72,10 @@ void  *__pmc_malloc  (size_t size) {
 }
 
 int __pmc_pobj_alloc(PMEMobjpool *pop, PMEMoid *oidp, uint64_t type_num, size_t size, pmemobj_constr constructor, void *arg){
-// int __pmc_pobj_alloc(PMEMobjpool *pop, TOID(struct Segment) *oidp, struct Segment, size_t size, pmemobj_constr constructor, void *arg) {
-    // PMEM_POBJ_ALLOC(pop, &dir, struct Directory, sizeof(struct Directory), NULL, NULL);
-    // ()<<oidp<<"\n";
-    // outs()<<type_num<<"\n";
     int ret = pmemobj_alloc(pop, oidp, type_num, size, constructor, arg);
 
     if (my_flag && ret == 0) {  // Check if allocation was successful and logging is enabled
-        void *ptr = pmemobj_direct(oidp->oid);   // Get a direct pointer to the allocated memory
+        void *ptr = pmemobj_oid(oidp);   // Get a direct pointer to the allocated memory
         pthread_mutex_lock(&mtx);
 
         // Log the address range
