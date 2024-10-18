9eda8ea0e38b9c57e5f91853b7dff8a6fb2b6217
KhanTooba <toobakha@usc.edu>
2024-10-17 17:11:58 -0700

    added comparitive analysis

--- repair/comparitive.sh
+++ repair/comparitive.sh
@@ -1,8 +1,28 @@
 mv Report.txt Report_original.txt
-touch Report.txt
-echo "NUMBER OF THREADS: 2">Report.txt
-python3 trace_formatter.py  ../experiments/fastFair/comparitive/fastFair_2.txt tempFastFair.txt
-python3 step1.py tempFastFair.txt tempFastFair1.txt fastFair
-python3 step4.py tempFastFair1.txt tempFastFair2.txt fastFair
+touch ComparitiveResults.txt
 
-# mv Report.txt Report_comparitive.txt
+for i in $(seq 2 2 32); do
+    input_file="../experiments/fastFair/comparitive/fastFair_$i.txt"
+    
+    if [ -f "$input_file" ]; then
+        python3 trace_formatter.py "$input_file" tempFastFair.txt
+        
+        t1=$(date +%s.%N)
+        python3 step1.py tempFastFair.txt tempFastFair1.txt fastFair
+        t2=$(date +%s.%N)
+        
+        python3 step4.py tempFastFair1.txt tempFastFair2.txt fastFair
+        t3=$(date +%s.%N)
+
+        execution_time_1=$(echo "$t2 - $t1" | bc)
+        execution_time_2=$(echo "$t3 - $t2" | bc)
+
+        echo "$i, $execution_time_1, $execution_time_2" >> ComparitiveResults.txt
+        rm tempFastFair.txt tempFastFair1.txt tempFastFair2.txt temp_results.txt
+    else
+        echo "File $input_file does not exist." >> ComparitiveResults.txt
+    fi
+done
+
+
+mv Report.txt Report_comparitiveAnalysis.txt
