13cf2dd26b7d8d57c4f6c117db1e5e64be1709aa
KhanTooba <toobakha@usc.edu>
2024-10-25 15:21:01 -0700

    added old pclht experiments to results section

--- repair/old-pclht.sh
+++
@@ -1,3 +0,0 @@
-# python3 trace_formatter.py ../experiments/p-clht_old/P-CLHT_output.txt ../experiments/p-clht_old/formatted_trace.txt
-# python3 step1.py ../experiments/p-clht_old/formatted_trace.txt ../experiments/p-clht_old/r1.txt P-CLHT
-python3 step4.py ../experiments/p-clht_old/r1.txt ../experiments/p-clht_old/r2.txt P-CLHT
