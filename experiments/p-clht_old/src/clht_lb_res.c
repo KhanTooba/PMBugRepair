fb7a91736594e675b023a04f22a3297a5ab1ca45
KhanTooba <tooba.khan@usc.edu>
2024-10-24 18:29:44 -0700

    New results

--- experiments/p-clht_old/src/clht_lb_res.c
+++ experiments/p-clht_old/src/clht_lb_res.c
@@ -231,9 +231,9 @@ clht_create(uint64_t num_buckets)
     // Open the PMEMpool if it exists, otherwise create it
     size_t pool_size = 32*1024*1024*1024UL;
     if (access("/dev/shm/pool", F_OK) != -1)
-        pop = pmemobj_open("/dev/shm/pool", POBJ_LAYOUT_NAME(clht));
+        pop = pmem_pmemobj_open("/dev/shm/pool", POBJ_LAYOUT_NAME(clht));
     else
-        pop = pmemobj_create("/dev/shm/pool", POBJ_LAYOUT_NAME(clht), pool_size, 0666);
+        pop = pmem_pmemobj_create("/dev/shm/pool", POBJ_LAYOUT_NAME(clht), pool_size, 0666);
 
     if (pop == NULL)
         perror("failed to open the pool\n");
@@ -458,7 +458,7 @@ clht_put(clht_t* h, clht_addr_t key, clht_val_t val)
         }
 
         int resize = 0;
-        if (likely(clht_ptr_from_off(bucket->next_off, true) == NULL))
+        if (1)//likely(clht_ptr_from_off(bucket->next_off, true) == NULL))
         {
             if (unlikely(empty == NULL))
             {
@@ -490,7 +490,7 @@ clht_put(clht_t* h, clht_addr_t key, clht_val_t val)
             }
 
             LOCK_RLS(lock);
-            if (unlikely(resize))
+            if (1)//unlikely(resize))
             {
                 /* ht_resize_pes(h, 1); */
 				DEBUG_PRINT("Calling ht_status for key %ld\n", (long)key);
